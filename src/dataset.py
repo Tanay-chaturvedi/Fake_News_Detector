452405d8aafc86a60af3d9cc1d58cc9ec27ed837
Tanay Chaturvedi <tanaychaturvedi126@gmail.com>
2026-08-04 01:54:04 +0530

    Create dataset pipeline for DistilBERT training

---
+++ src/dataset.py
@@ -0,0 +1,43 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class NewsDataset(Dataset):
+
+    def __init__(self, dataframe, tokenizer, max_length=128):
+
+        self.dataframe = dataframe
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+
+        statement = str(
+            self.dataframe.iloc[index]["statement"]
+        )
+
+        label = self.dataframe.iloc[index]["label"]
+
+        encoding = self.tokenizer(
+            statement,
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+            return_tensors="pt"
+        )
+
+        return {
+
+            "input_ids":
+                encoding["input_ids"].squeeze(0),
+
+            "attention_mask":
+                encoding["attention_mask"].squeeze(0),
+
+            "label":
+                torch.tensor(label, dtype=torch.long)
+        }
