0be13c2cf26437358e0f1f68ecdf19c2fdb4b641
Tanay Chaturvedi <tanaychaturvedi126@gmail.com>
2026-08-04 11:49:37 +0530

    Implemented DistilBERT model training and evaluation

--- src/dataset.py
+++ src/dataset.py
@@ -4,26 +4,21 @@ from torch.utils.data import Dataset
 
 class NewsDataset(Dataset):
 
-    def __init__(self, dataframe, tokenizer, max_length=128):
-
+    def __init__(self, dataframe, tokenizer, max_length=256):
         self.dataframe = dataframe
         self.tokenizer = tokenizer
         self.max_length = max_length
 
     def __len__(self):
-
         return len(self.dataframe)
 
     def __getitem__(self, index):
 
-        statement = str(
-            self.dataframe.iloc[index]["statement"]
-        )
-
+        text = str(self.dataframe.iloc[index]["content"])
         label = self.dataframe.iloc[index]["label"]
 
         encoding = self.tokenizer(
-            statement,
+            text,
             truncation=True,
             padding="max_length",
             max_length=self.max_length,
@@ -31,13 +26,7 @@ class NewsDataset(Dataset):
         )
 
         return {
-
-            "input_ids":
-                encoding["input_ids"].squeeze(0),
-
-            "attention_mask":
-                encoding["attention_mask"].squeeze(0),
-
-            "label":
-                torch.tensor(label, dtype=torch.long)
+            "input_ids": encoding["input_ids"].squeeze(0),
+            "attention_mask": encoding["attention_mask"].squeeze(0),
+            "label": torch.tensor(label, dtype=torch.long)
         }
